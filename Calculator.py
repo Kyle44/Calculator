e939b190daa596fa647b66b6d8035ee3ad05e530
Kyle44 <kyle44@comcast.net>
2015-02-24 11:29:46 -0500

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -2,13 +2,15 @@
 # Written by:  Kyle Fritz
 # Date:        9/19/2013
 # Lab Section: 10
+############### USE WITH PYTHON 3 ###########
+# scl enable python33 bash
 # Description: This program adds, subtracts, multiplies, and divides any two
 #    numbers (besides division by zero).
 print("This program will add, subtract, multiply, or divide any two integers.")
 def main():
     int1 = (input("Type in your first operand: "))
     int2 = (input("Type in your second operand: "))
-    operAtion = (raw_input("Enter your operation: "))
+    operAtion = (input("Enter your operation: "))
     a = int(int1)  # This casts the first operand as an integer.
     b = int(int2)  # This casts the second operand as an integer.
     print("The first operand is", a)
